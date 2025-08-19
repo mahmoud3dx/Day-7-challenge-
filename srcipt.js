b87cfc317894e3752d5047a725998a8ff4d36724
Mohamud Mohamud <xxhmod.com@gmail.com>
2025-08-19 19:42:13 +0300

    DONE

---
+++ srcipt.js
@@ -0,0 +1,11 @@
+let name;
+let age;
+const city = "";
+
+console.log("Enter your name");
+console.log(name = prompt());
+
+console.log("Enter your age");
+console.log(age = prompt());
+
+alert("Hello " + name + ", your age is " + age);
